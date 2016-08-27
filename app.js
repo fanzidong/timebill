4b769a3848ce9c472a5e529ba7a051df5abd0ae9
fanzidong <450323391@qq.com>
2016-08-27 13:08:01 +0800

    convert jade to ejs
    Signed-off-by: fanzidong <450323391@qq.com>

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ var app = module.exports = express();
 // all environments
 app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
   extended: true
