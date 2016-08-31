1782a02d5f7ac682b0de78b5fef6705f9038f7c6
fanzidong <450323391@qq.com>
2016-08-31 22:46:59 +0800

    录入数据
    Signed-off-by: fanzidong <450323391@qq.com>

--- db/billtype.sql
+++ db/billtype.sql
@@ -1,29 +1,29 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : nm3000
-Source Server Version : 50614
-Source Host           : localhost:3003
+Source Server         : localhost_3306
+Source Server Version : 50624
+Source Host           : localhost:3306
 Source Database       : timebill
 
 Target Server Type    : MYSQL
-Target Server Version : 50614
+Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2016-08-31 17:15:14
+Date: 2016-08-31 22:46:13
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `billtype`
+-- Table structure for billtype
 -- ----------------------------
 DROP TABLE IF EXISTS `billtype`;
 CREATE TABLE `billtype` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of billtype
@@ -39,3 +39,4 @@ INSERT INTO `billtype` VALUES ('8', '户外');
 INSERT INTO `billtype` VALUES ('9', '体育');
 INSERT INTO `billtype` VALUES ('10', '思考');
 INSERT INTO `billtype` VALUES ('14', '教育');
+INSERT INTO `billtype` VALUES ('15', '游戏');
